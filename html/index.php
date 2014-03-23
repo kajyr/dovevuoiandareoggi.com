14f5e62dbbbc0f98d48697c7824d7ae6e4668af7
Carlo kajyr <carlo.panzi@gmail.com>
2014-03-23 18:27:33 +0100

    permalink

--- html/index.php
+++ html/index.php
@@ -2,7 +2,19 @@
 
 $lang = 'it';
 
-$frasi['it'][] = "affanculo."
+$frasi['it'] = array(
+	"affanculo."
+);
+
+$pos = strip_tags($_GET['a']);
+
+if ( empty($pos) || empty($frasi[$lang][$pos]) ) {
+
+	//$pos = 0;
+	$pos = array_rand($frasi[$lang]);
+}
+
+
 
 ?>
 
@@ -11,81 +23,93 @@ $frasi['it'][] = "affanculo."
 
 <html lang="<?php echo $lang; ?>">
 <head>
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-  <meta http-equiv="Content-Language" content="<?php echo $lang; ?>" />
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<meta http-equiv="Content-Language" content="<?php echo $lang; ?>" />
 
-  <title>Dove vuoi andare oggi?</title>
-  <meta name="description" content="Dove vuoi andare oggi?">
-  <meta name="author" content="Carlo 'kajyr'">
+	<title>Dove vuoi andare oggi?</title>
+	<meta name="description" content="Dove vuoi andare oggi?">
+	<meta name="author" content="Carlo 'kajyr'">
 
-  <?php /*<meta name="viewport" content="width=device-width, initial-scale=1.0">*/?>
-  
-  <?php /* <link rel="shortcut icon" href="/favicon.ico">
-  <link rel="apple-touch-icon" href="/apple-touch-icon.png">*/?>
+	<?php /*<meta name="viewport" content="width=device-width, initial-scale=1.0">*/?>
+	
+	<?php /* <link rel="shortcut icon" href="/favicon.ico">
+	<link rel="apple-touch-icon" href="/apple-touch-icon.png">*/?>
 
-  <link href='http://fonts.googleapis.com/css?family=Lobster+Two:700' rel='stylesheet' type='text/css'>
-  <link rel="stylesheet" href="css/style.css?v=2">
+	<link href='http://fonts.googleapis.com/css?family=Lobster+Two:700' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" href="css/style.css?v=2">
 
-  <link rel="author" type="text/html" href="http://google.com/+CarloPanzi"/>
+	<link rel="author" type="text/html" href="http://google.com/+CarloPanzi"/>
 
-  <meta property="fb:admins" content="707570570" />
-  <meta property="og:title" content="Dove vuoi andare oggi?"/>
-  <meta property="og:type" content="website"/>
-  <meta property="og:site_name" content="Dove vuoi andare oggi?"/>
-  <meta property="og:url" content="http://dovevuoiandareoggi.com">
-  <meta property="og:description" content="Dove vuoi andare oggi ti da degli ottimi suggerimenti"/>
+	<meta property="fb:admins" content="707570570" />
+	<meta property="og:title" content="Dove vuoi andare oggi?"/>
+	<meta property="og:type" content="website"/>
+	<meta property="og:site_name" content="Dove vuoi andare oggi?"/>
+	<meta property="og:url" content="http://dovevuoiandareoggi.com">
+	<meta property="og:description" content="Dove vuoi andare oggi ti da degli ottimi suggerimenti"/>
 
 </head>
 
 <body>
 
-  <div id="container">
-    <header>
+	<div id="container">
+		<header>	
+		</header>
 
-    </header>
-    <div id="main" role="main">
+		<section id="suggestion">
+			<p>
+				<?php echo $frasi[$lang][$pos]; ?>
+			</p>
+		</section>
 
-      <p>
-        <?php echo $frasi[$lang][array_rand($frasi[$lang])] ?>
-      </p>
 
-    </div>
-    <footer>
-      <div class="social">
-        <div id="fb-root"></div><script src="http://connect.facebook.net/en_US/all.js#appId=176793495722085&amp;xfbml=1"></script>
-        <fb:like href="" send="true" layout="button_count" width="150" show_faces="false" action="recommend" font=""></fb:like>
-      </div>
-      <!-- Place this tag where you want the +1 button to render. -->
-      <div class="social">
-        <div class="g-plusone" data-size="medium"></div>
-      </div>
-    </footer>
-    </div>
 
-    <!-- Place this tag after the last +1 button tag. -->
-    <script type="text/javascript">
-    window.___gcfg = {lang: 'it'};
+		<footer>
 
-    (function() {
-      var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
-      po.src = 'https://apis.google.com/js/plusone.js';
-      var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
-    })();
-    </script>
 
-    <script type="text/javascript">
 
-    var _gaq = _gaq || [];
-    _gaq.push(['_setAccount', 'UA-1163483-10']);
-    _gaq.push(['_trackPageview']);
 
-    (function() {
-      var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-      ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-      var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-    })();
+			<div class="social" id="facebook">
+				<div id="fb-root"></div><script src="http://connect.facebook.net/en_US/all.js#appId=176793495722085&amp;xfbml=1"></script>
+				<fb:like href="" send="true" layout="button_count" width="150" show_faces="false" action="recommend" font=""></fb:like>
+			</div>
+			<!-- Place this tag where you want the +1 button to render. -->
+			<div class="social" id="google">
+				<div class="g-plusone" data-size="medium"></div>
+			</div>
 
-    </script>
-  </body>
-  </html>
+			<section id="links">
+				<a href="/?a=<?php echo $pos; ?>">permalink</a> | 
+				suggest |
+				proudly made by <a href="https://github.com/kajyr">@kajyr</a>
+			</section>
+
+		</footer>
+	</div>
+
+	<!-- Place this tag after the last +1 button tag. -->
+	<script type="text/javascript">
+	window.___gcfg = {lang: 'it'};
+
+	(function() {
+		var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
+		po.src = 'https://apis.google.com/js/plusone.js';
+		var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+	})();
+	</script>
+
+	<script type="text/javascript">
+
+	var _gaq = _gaq || [];
+	_gaq.push(['_setAccount', 'UA-1163483-10']);
+	_gaq.push(['_trackPageview']);
+
+	(function() {
+		var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+		ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+		var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+	})();
+
+	</script>
+</body>
+</html>
