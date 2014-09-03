7fa5aed79a04c97dfd1004c21f7f6f2db682160c
Carlo kajyr <carlo.panzi@gmail.com>
2014-09-03 16:51:37 +0200

    build e pulizia

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,6 +3,13 @@ module.exports = function(grunt) {
 	// Project configuration.
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
+		compass: {
+			release: {
+				options: {
+					config: 'config.rb'
+				}
+			}
+		},
 		ftp_push: {
 
 			prod: {
@@ -17,7 +24,7 @@ module.exports = function(grunt) {
 			}
 		}
 	});
-
+	grunt.loadNpmTasks('grunt-contrib-compass');
 	grunt.loadNpmTasks('grunt-ftp-push');
 
 	// Default task(s).
