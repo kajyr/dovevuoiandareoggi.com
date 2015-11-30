d2c5fc4198f1e99d206cb05ce835ae169c7ca66d
kajyr <carlo.panzi@gmail.com>
2015-11-30 17:16:33 +0100

    spostato tutto

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,32 +3,19 @@ module.exports = function(grunt) {
 	// Project configuration.
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
-		compass: {
+		sass: {
 			release: {
 				options: {
 					config: 'config.rb'
 				}
 			}
-		},
-		ftp_push: {
-
-			prod: {
-				options: {
-					host: 'ftp.s85630.gridserver.com',
-					authKey: 'nightdrops',
-					dest: '/domains/dovevuoiandareoggi.com/'
-				},
-				files: [
-					{ expand: true, src: ['html/**'] }
-				]
-			}
 		}
 	});
-	grunt.loadNpmTasks('grunt-contrib-compass');
+	grunt.loadNpmTasks('grunt-sass');
 	grunt.loadNpmTasks('grunt-ftp-push');
 
 	// Default task(s).
 	grunt.registerTask('build', ['compass']);
-	grunt.registerTask('deploy', ['build', 'ftp_push']);
+	grunt.registerTask('deploy', ['build', '']);
 
 };
