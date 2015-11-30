2715bf4713ec68247b41a564fdc816f9425a2e97
kajyr <carlo.panzi@gmail.com>
2015-11-30 17:21:31 +0100

    libsass

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,9 +4,12 @@ module.exports = function(grunt) {
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
 		sass: {
-			release: {
-				options: {
-					config: 'config.rb'
+			options: {
+				sourceMap: false
+			},
+			dist: {
+				files: {
+					'css/style.css': 'sass/style.scss'
 				}
 			}
 		}
@@ -15,7 +18,7 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-ftp-push');
 
 	// Default task(s).
-	grunt.registerTask('build', ['compass']);
+	grunt.registerTask('build', ['sass']);
 	grunt.registerTask('deploy', ['build', '']);
 
 };
